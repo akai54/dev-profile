50c154fd56c32d46131e6143527c68b1eb9e7608
besher al addam <beshoux@outlook.fr>
2024-06-18 21:58:13 +0200

    react-profile-card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="pp.jpeg" alt="avatar" style={{ width: "100%" }} />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Besher AL ADDAM</h1>
+      <p>Développeur Full Stack</p>
+    </div>
+  );
+}
+
+function SkillList(props) {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML" emoji="👀" color="red" />
+      <Skill skill="CSS" emoji="🌒" color="orange" />
+      <Skill skill="JavaScript" emoji="🎹" color="yellow" />
+      <Skill skill="React" emoji="🀄" color="cyan" />
+      <Skill skill="Node" emoji="😎" color="green" />
+      <Skill skill="Express" emoji="🎴" color="grey" />
+      <Skill skill="MongoDB" emoji="🃏" color="#568f79" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
